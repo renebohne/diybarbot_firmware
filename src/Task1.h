d5757b6d8de36e6b670200b5170be49d6824c168
renebohne <rene.bohne@gmail.com>
2024-06-28 23:20:51 +0200

    VL53L0X via I2C

--- src/Task1.h
+++ src/Task1.h
@@ -4,23 +4,35 @@ int glassDistance = 0;
 int messurment = 0;
 int aux = 0;
 
-void workLoad (void) ;     
+void workLoad(void);
 
-void Task1( void * parameter ) {
-  // Everything defined on the setup
-   pinMode(PWM_PIN, INPUT);
-   for (;;) {  
-     // Everything defined on the loop
-     messurment = pulseIn(PWM_PIN, HIGH);
-     if ((messurment < minGlassDistance && messurment > 0) || (messurment > minGlassDistance && aux > 1)) {
-        glassDistance = messurment;
-        aux = 0;    
-     } else if (glassDistance > 0) {
-        aux++;
-     }
-     //glassDistance = 700;//comment out if you don't have a distance sensor.
-     delay(100);
+
+
+void Task1(void *parameter)
+{
+
+#ifdef USE_IICSENSOR
+   for (;;)
+   {
+      delay(100);
    }
-   
-
+#else
+   pinMode(PWM_PIN, INPUT);
+   for (;;)
+   {
+      // Everything defined on the loop
+      messurment = pulseIn(PWM_PIN, HIGH);
+      if ((messurment < minGlassDistance && messurment > 0) || (messurment > minGlassDistance && aux > 1))
+      {
+         glassDistance = messurment;
+         aux = 0;
+      }
+      else if (glassDistance > 0)
+      {
+         aux++;
+      }
+      //glassDistance = 700; // comment out if you don't have a distance sensor.
+      delay(100);
+   }
+#endif
 }
